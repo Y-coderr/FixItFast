bd4c049b6fa9c2b74d25b5b17a9af8fa9ef10b6f
Yash Kadam <kadamyash6112@gmail.com>
2024-02-10 22:27:25 +0530

    signup page --yash kadam

---
+++ app/src/main/java/com/example/fixitfast/SignupActivity.java
@@ -0,0 +1,140 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		galileo_design
+	 *	@date 		Saturday 10th of February 2024 02:54:34 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.fixitfast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class galileo_design_activity extends Activity {
+
+	
+	private View _bg__galileo_design_ek2;
+	private View _bg__depth_0__frame_0_ek1;
+	private View _bg__depth_1__frame_2_ek1;
+	private View _bg__depth_2__frame_0_ek1;
+	private View _bg__depth_3__frame_0_ek1;
+	private View _bg__depth_4__frame_0_ek1;
+	private View _bg__depth_5__frame_0_ek1;
+	private View _bg__depth_6__frame_0_ek1;
+	private View _bg__depth_7__frame_0_ek1;
+	private TextView username;
+	private View _bg__depth_1__frame_3_ek1;
+	private View _bg__depth_2__frame_0_ek3;
+	private View _bg__depth_3__frame_0_ek3;
+	private View _bg__depth_4__frame_0_ek3;
+	private View _bg__depth_5__frame_0_ek3;
+	private View _bg__depth_6__frame_0_ek3;
+	private View _bg__depth_7__frame_0_ek3;
+	private TextView email;
+	private View _bg__depth_1__frame_4_ek1;
+	private View _bg__depth_2__frame_0_ek5;
+	private View _bg__depth_3__frame_0_ek5;
+	private View _bg__depth_4__frame_0_ek5;
+	private View _bg__depth_5__frame_0_ek5;
+	private View _bg__depth_6__frame_0_ek5;
+	private View _bg__depth_7__frame_0_ek5;
+	private TextView password;
+	private View _bg__depth_6__frame_1_ek1;
+	private View _bg__depth_7__frame_0_ek7;
+	private ImageView vector___0;
+	private View _bg__depth_8__frame_0_ek1;
+	private View _bg__depth_1__frame_5_ek1;
+	private View _bg__depth_2__frame_0_ek7;
+	private View _bg__depth_3__frame_0_ek7;
+	private View _bg__depth_4__frame_0_ek7;
+	private View _bg__depth_5__frame_0_ek7;
+	private View _bg__depth_6__frame_0_ek7;
+	private View _bg__depth_7__frame_0_ek9;
+	private TextView confirm_password;
+	private View _bg__depth_6__frame_1_ek3;
+	private View _bg__depth_7__frame_0_ek11;
+	private ImageView vector___0_ek1;
+	private View _bg__depth_8__frame_0_ek3;
+	private View _bg__depth_1__frame_6_ek1;
+	private View _bg__depth_2__frame_0_ek9;
+	private View _bg__depth_3__frame_0_ek9;
+	private View _bg__depth_4__frame_0_ek9;
+	private TextView sign_up;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_Signup);
+
+		
+		_bg__galileo_design_ek2 = (View) findViewById(R.id._bg__galileo_design_ek2);
+		_bg__depth_0__frame_0_ek1 = (View) findViewById(R.id._bg__depth_0__frame_0_ek1);
+		_bg__depth_1__frame_2_ek1 = (View) findViewById(R.id._bg__depth_1__frame_2_ek1);
+		_bg__depth_2__frame_0_ek1 = (View) findViewById(R.id._bg__depth_2__frame_0_ek1);
+		_bg__depth_3__frame_0_ek1 = (View) findViewById(R.id._bg__depth_3__frame_0_ek1);
+		_bg__depth_4__frame_0_ek1 = (View) findViewById(R.id._bg__depth_4__frame_0_ek1);
+		_bg__depth_5__frame_0_ek1 = (View) findViewById(R.id._bg__depth_5__frame_0_ek1);
+		_bg__depth_6__frame_0_ek1 = (View) findViewById(R.id._bg__depth_6__frame_0_ek1);
+		_bg__depth_7__frame_0_ek1 = (View) findViewById(R.id._bg__depth_7__frame_0_ek1);
+		username = (TextView) findViewById(R.id.username);
+		_bg__depth_1__frame_3_ek1 = (View) findViewById(R.id._bg__depth_1__frame_3_ek1);
+		_bg__depth_2__frame_0_ek3 = (View) findViewById(R.id._bg__depth_2__frame_0_ek3);
+		_bg__depth_3__frame_0_ek3 = (View) findViewById(R.id._bg__depth_3__frame_0_ek3);
+		_bg__depth_4__frame_0_ek3 = (View) findViewById(R.id._bg__depth_4__frame_0_ek3);
+		_bg__depth_5__frame_0_ek3 = (View) findViewById(R.id._bg__depth_5__frame_0_ek3);
+		_bg__depth_6__frame_0_ek3 = (View) findViewById(R.id._bg__depth_6__frame_0_ek3);
+		_bg__depth_7__frame_0_ek3 = (View) findViewById(R.id._bg__depth_7__frame_0_ek3);
+		email = (TextView) findViewById(R.id.email);
+		_bg__depth_1__frame_4_ek1 = (View) findViewById(R.id._bg__depth_1__frame_4_ek1);
+		_bg__depth_2__frame_0_ek5 = (View) findViewById(R.id._bg__depth_2__frame_0_ek5);
+		_bg__depth_3__frame_0_ek5 = (View) findViewById(R.id._bg__depth_3__frame_0_ek5);
+		_bg__depth_4__frame_0_ek5 = (View) findViewById(R.id._bg__depth_4__frame_0_ek5);
+		_bg__depth_5__frame_0_ek5 = (View) findViewById(R.id._bg__depth_5__frame_0_ek5);
+		_bg__depth_6__frame_0_ek5 = (View) findViewById(R.id._bg__depth_6__frame_0_ek5);
+		_bg__depth_7__frame_0_ek5 = (View) findViewById(R.id._bg__depth_7__frame_0_ek5);
+		password = (TextView) findViewById(R.id.password);
+		_bg__depth_6__frame_1_ek1 = (View) findViewById(R.id._bg__depth_6__frame_1_ek1);
+		_bg__depth_7__frame_0_ek7 = (View) findViewById(R.id._bg__depth_7__frame_0_ek7);
+		vector___0 = (ImageView) findViewById(R.id.vector___0);
+		_bg__depth_8__frame_0_ek1 = (View) findViewById(R.id._bg__depth_8__frame_0_ek1);
+		_bg__depth_1__frame_5_ek1 = (View) findViewById(R.id._bg__depth_1__frame_5_ek1);
+		_bg__depth_2__frame_0_ek7 = (View) findViewById(R.id._bg__depth_2__frame_0_ek7);
+		_bg__depth_3__frame_0_ek7 = (View) findViewById(R.id._bg__depth_3__frame_0_ek7);
+		_bg__depth_4__frame_0_ek7 = (View) findViewById(R.id._bg__depth_4__frame_0_ek7);
+		_bg__depth_5__frame_0_ek7 = (View) findViewById(R.id._bg__depth_5__frame_0_ek7);
+		_bg__depth_6__frame_0_ek7 = (View) findViewById(R.id._bg__depth_6__frame_0_ek7);
+		_bg__depth_7__frame_0_ek9 = (View) findViewById(R.id._bg__depth_7__frame_0_ek9);
+		confirm_password = (TextView) findViewById(R.id.confirm_password);
+		_bg__depth_6__frame_1_ek3 = (View) findViewById(R.id._bg__depth_6__frame_1_ek3);
+		_bg__depth_7__frame_0_ek11 = (View) findViewById(R.id._bg__depth_7__frame_0_ek11);
+		vector___0_ek1 = (ImageView) findViewById(R.id.vector___0_ek1);
+		_bg__depth_8__frame_0_ek3 = (View) findViewById(R.id._bg__depth_8__frame_0_ek3);
+		_bg__depth_1__frame_6_ek1 = (View) findViewById(R.id._bg__depth_1__frame_6_ek1);
+		_bg__depth_2__frame_0_ek9 = (View) findViewById(R.id._bg__depth_2__frame_0_ek9);
+		_bg__depth_3__frame_0_ek9 = (View) findViewById(R.id._bg__depth_3__frame_0_ek9);
+		_bg__depth_4__frame_0_ek9 = (View) findViewById(R.id._bg__depth_4__frame_0_ek9);
+		sign_up = (TextView) findViewById(R.id.sign_up);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
