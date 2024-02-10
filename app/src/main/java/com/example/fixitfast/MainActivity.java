a0a5bc21e5992beb6e9db17ed8ffb7d4a391cdd6
Yash Kadam <kadamyash6112@gmail.com>
2024-02-10 20:11:57 +0530

    SplashScreen and firebase connection --yash kadam

--- app/src/main/java/com/example/fixitfast/MainActivity.java
+++ app/src/main/java/com/example/fixitfast/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.fixitfast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/example/fixitfast/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.fixitfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        setContentView(R.layout.activity_splash);
+        Intent iHome = new Intent(SplashActivity.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(iHome);
+            }
+        },4000);
+
+    }
+}
